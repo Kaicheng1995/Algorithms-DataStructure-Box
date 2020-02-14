bd02971de401f865d274afc67d0d0b8855be34d4
Kaicheng Jia <ares.jia@hotmail.com>
2020-02-14 15:22:33 +0800

    Update Simple Multification.java

--- Simple Multification.java
+++ Simple Multification.java
@@ -2,7 +2,6 @@
 
 https://yordigital.com/blog/big-o-notation-traditional-grade-school-algorithms-addition-multiplication/
 
-
 Above link shows the Big-O notation value of simple multification. For each input "N", we have mostly "2N" operations in every 
 Line, and "N" lines in every rows. Also notice that the last step is to add all the results, but here the operations are only 
 constant. Thus, the Big-O of simple multification is "N²".
