1f47941f98fe9311e90ccbd70e6581386e19561a
Kaicheng Jia <ares.jia@hotmail.com>
2020-02-25 11:59:01 +0800

    Update Fib.java

--- Discussion/Fib.java
+++ Discussion/Fib.java
@@ -11,3 +11,19 @@ public class Fib {
         }
     }
 }
+
+
+
+// ???????
+public class Fib {
+    public static void main(String[] args) {
+        System.out.println(fib2(1,1,1,1));
+    }
+
+    public static int fib2(int n, int k, int f0, int f1) {
+        if (n == k) {
+            return f0; }
+        else {
+            return fib2(n, k + 1, f1, f0 + f1); }
+    }
+}
